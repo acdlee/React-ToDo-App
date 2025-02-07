ec757f91e79278693d84a2920012ef3a186d76ba
christopher-lee <clee651@terpmail.umd.edu>
2025-02-07 13:26:37 -0500

    Fix bug where updating a 'striked' todo persists the strike, but not the checkbox: make checkbox persist after todo update.

--- src/components/todo-items/TodoItem.jsx
+++ src/components/todo-items/TodoItem.jsx
@@ -8,7 +8,7 @@ const CROSSSTYLE = {
 function TodoItem({ todoIndex, text, isCompleted, onCheckboxClick, onDeleteClick, onEditClick }) {
     return (
         <li style={isCompleted ? CROSSSTYLE : {}}>
-            <input type="checkbox" onClick={() => onCheckboxClick(todoIndex)} />
+            <input type="checkbox" onClick={() => onCheckboxClick(todoIndex)} defaultChecked={isCompleted} />
             {text}
             <button onClick={() => onDeleteClick(todoIndex)}>X</button>
             <button onClick={() => onEditClick(todoIndex)}>Edit</button>
